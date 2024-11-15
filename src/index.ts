1bf53c7c04f1422ddb5e3e8b4ee73b71a48caacb
yikkai <yikkai95@gmail.com>
2024-11-16 03:50:55 +0800

    feat: wire sign protocol

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import 'dotenv/config'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { ADMIN } from './util.js'
+import { ADMIN, SignClient } from './util.js'
+import fs from 'fs'
 const app = new Hono()
 
 app.use('*', cors({
@@ -16,6 +17,10 @@ app.use('*', cors({
 app.post('/lit', async (c) => {
   const body = await c.req.json()
   console.log(body)
+  // {
+    //   eoa: '0x915B5A9a2485FDb2B68f06Cb21F09aF272ba7b08',
+    //   owner: '0x12a2909c0F1fC6261DD7b940F6dB6f3b2C7aa83D'
+    // }
   // await fs.writeFile('/tmp/body.json', JSON.stringify(body, null, 2))
   return c.json({verified: true})
 })
@@ -23,7 +28,20 @@ app.post('/lit', async (c) => {
 app.post('/connect', async (c) => {
   const body = await c.req.json()
   console.log(body)
+  fs.writeFileSync('/tmp/body.json', JSON.stringify(body, null, 2))
 
+  const res = await SignClient.createAttestation(body.sign_protocol_info.attestation, {
+    delegationSignature: body.sign_protocol_info.delegationSignature,
+  })
+  console.log(res)
+  
+  //   const delegationCreateAttestationRes = await client.createAttestation(
+  //   info.attestation,
+  //   {
+  //     delegationSignature: info.delegationSignature,
+  //   }
+  // );
+  // console.log(delegationCreateAttestationRes);
   return c.json({body})
 })
 
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,25 @@
 import { privateKeyToAccount } from "viem/accounts"
 
+import {
+    SignProtocolClient,
+    SpMode,
+    EvmChains,
+    IndexService,
+    decodeOnChainData,
+    DataLocationOnChain,
+    delegateSignAttestation
+    // delegateSignAttestation,
+    // delegateSignRevokeAttestation,
+    // delegateSignSchema,
+} from "@ethsign/sp-sdk";
+
+import { generatePrivateKey } from "viem/accounts";
+
 export const ADMIN = privateKeyToAccount(process.env.ADMIN as `0x${string}`)
 
-console.log(ADMIN.address)
+// console.log(ADMIN.address)
+
+export const SignClient = new SignProtocolClient(SpMode.OnChain, {
+    chain: EvmChains.baseSepolia,
+    account: ADMIN, // Optional if you are using an injected provider
+});
