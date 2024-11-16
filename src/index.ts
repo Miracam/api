4d444ae80bea99c6b4836b495455a67263001526
yikkai <yikkai95@gmail.com>
2024-11-16 11:35:01 +0800

    feat: mint nft upon upload

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import * as Attester from './attester.js'
 
 
 import fs from 'fs'
+import { mintCamNFT } from './nft.js'
 const app = new Hono()
 
 app.use('*', cors({
@@ -22,6 +23,15 @@ app.use('*', cors({
 }))
 
 
+app.post('/publish', async (c) => {
+  console.log('publish')
+  let body = await c.req.json()
+  fs.writeFileSync('/tmp/metadata.json', JSON.stringify(body, null, 2))
+
+  const tx = await mintCamNFT(body)
+  return c.json({ tx: tx.hash, success: true })
+})
+
 app.post('/upload', async (c) => {
   console.log('upload')
   // print the body
@@ -31,7 +41,6 @@ app.post('/upload', async (c) => {
   // upload iamge data to irys
 //{  "sha256": "eb121b1a612363f83b78ac03d06e8bb0269c5ea2b3bddae68eb9f227743ce050",  "eth": {    "signature": "ccc2a0c163be0b6821379de4cde623fc7d03b2da3debb284027fd28d01e5b07c58c619510e975cbff041a209c3bffdbbc5480b151690537fc266725be4147c851c",    "pubkey": "0xDF1fa57240Ee6896F063abb0276cfe110fE34796"  },  "secp256r1": {    "pubkey": "BPHPmNPA8EViaYQ58MaWs2sy3VkWiyhU0HJKDmMDlzxhSkW+FG2uSGnm9R4nsDcZYeMy1V64zE5nFGnX8z3IXZg=",    "signature": "MEUCIGIt3PLlsr4aJKJ0r7QCOEcYvH4gbBzWNGT9TfPvi4ZEAiEAjY3WkdwC9p69m+VMh1UAyAsFEyERiLOySbuWBlisxZw="  },  "content": {    "type": "public",    "value": {      "metadata": {        "deviceInfo": "[\"model\": \"iPhone 13 Pro\", \"systemVersion\": \"18.1\", \"name\": \"iPhone\"]",        "timestamp": "2024-11-07T06:21:32Z",        "imageProperties": "[\"EXIF\": [\"SceneType\": 1, \"LensMake\": Apple, \"PixelYDimension\": 1080, \"DateTimeOriginal\": 2024:11:07 14:21:31, \"CustomRendered\": 1, \"FNumber\": 1.5, \"ApertureValue\": 1.169925002106682, \"PixelXDimension\": 1920, \"FocalLength\": 5.7, \"Flash\": 16, \"ExposureMode\": 0, \"FocalLenIn35mmFilm\": 27, \"SubjectArea\": <__NSArrayM 0x301139b90>(\n960,\n539,\n1056,\n475\n)\n, \"ExposureProgram\": 2, \"OffsetTime\": +08:00, \"ExifVersion\": <__NSArrayM 0x301138d50>(\n2,\n3,\n2\n)\n, \"ExposureBiasValue\": 0, \"ComponentsConfiguration\": <__NSArrayM 0x30113bbd0>(\n1,\n2,\n3,\n0\n)\n, \"FlashPixVersion\": <__NSArrayM 0x30113b1e0>(\n1,\n0\n)\n, \"ShutterSpeedValue\": 5.644000467344316, \"OffsetTimeDigitized\": +08:00, \"ISOSpeedRatings\": <__NSArrayM 0x30113a4c0>(\n400\n)\n, \"OffsetTimeOriginal\": +08:00, \"ExposureTime\": 0.02, \"SubsecTimeDigitized\": 862, \"BrightnessValue\": 0.397811839047445, \"WhiteBalance\": 0, \"LensSpecification\": <__NSArrayM 0x30113a040>(\n5.7,\n5.7,\n1.5,\n1.5\n)\n, \"LensModel\": iPhone 13 Pro back camera 5.7mm f/1.5, \"MeteringMode\": 5, \"SubsecTimeOriginal\": 862, \"SensingMethod\": 2, \"ColorSpace\": 1, \"SceneCaptureType\": 0, \"DateTimeDigitized\": 2024:11:07 14:21:31], \"Depth\": 8, \"PixelHeight\": 1080, \"TIFF\": [\"DateTime\": 2024:11:07 14:21:31, \"ResolutionUnit\": 2, \"XResolution\": 72, \"Model\": iPhone 13 Pro, \"YResolution\": 72, \"Make\": Apple, \"Orientation\": 6, \"Software\": 18.1, \"HostComputer\": iPhone 13 Pro], \"ColorModel\": RGB, \"PixelWidth\": 1920, \"DPIHeight\": 72, \"Orientation\": 6, \"DPIWidth\": 72]",        "sensorData": "[\"latitude\": 1.4857496469926452, \"timestamp\": 1730960492.1406, \"heading\": 19.428152084350586, \"decibels\": 47.209156, \"batteryLevel\": 55, \"motion\": [\"pitch\": 55.072353403177964, \"yaw\": -1.9673710735239849, \"gravity\": [\"z\": -0.5714854001998901, \"x\": -0.03475777059793472, \"y\": -0.8198757171630859], \"roll\": -3.480445249422036], \"deviceModel\": \"iPhone 13 Pro\", \"altitude\": 17.63135796146054, \"longitude\": 103.67958559668226]"      }    }  }}
   let metadataUrl = ""
-  let imageUrl = ""
   const raw = JSON.stringify(body)
   fs.writeFileSync('/tmp/raw.json', raw)
 
@@ -74,12 +83,11 @@ app.post('/upload', async (c) => {
     console.log("uploading image")  
     const img = Buffer.from(body.content.value.mediadata, 'base64')
     const imgResult = await Irys.irys.uploadData(img, [{ name: "content-type", value: "image/png" }])
-    imageUrl = imgResult.url
 
     console.log("uploading metadata")
     const metadataResult = await Irys.irys.uploadData(JSON.stringify({
       attributes,
-      image: imageUrl,
+      image: imgResult.url,
       proof: {
         secp256r1: body.secp256r1,
         eth: body.eth
@@ -91,7 +99,6 @@ app.post('/upload', async (c) => {
 
   } else if (body.content.type === "private") {
     console.log('private')
-    //TODO: implement private upload
     fs.writeFileSync('/tmp/private.json', JSON.stringify(body))
 
     const metadataResult = await Irys.irys.uploadData(JSON.stringify({
@@ -106,7 +113,7 @@ app.post('/upload', async (c) => {
   }
   // let irysId = await Irys.irys.uploadData(JSON.stringify(body))
 
-
+  console.log({ url: metadataUrl, owner: body.eth.pubkey.toLowerCase() })
   return c.json({ url: metadataUrl, owner: body.eth.pubkey.toLowerCase() })
 
 })
---
+++ src/nft.ts
@@ -0,0 +1,87 @@
+import { ethers } from "ethers";
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const wallet = new ethers.Wallet(process.env.ADMIN!, provider)
+
+const minter = new ethers.Contract(process.env.MINTER_CONTRACT_ADDRESS!, [
+  "function mint(uint r, uint s, bytes pubkey, address to, string url) public"
+], wallet);
+
+async function extract(pubkey: string, signature: string) {
+  const pubKeyBuf = Buffer.from(pubkey, 'base64')
+  const signatureBuffer = Buffer.from(signature, 'base64')
+
+  const x = pubKeyBuf.subarray(1, 33)
+  const y = pubKeyBuf.subarray(33)
+
+  const [r, s] = derToRS(signatureBuffer)
+
+  return { x, y, r, s }
+}
+
+function derToRS(der: Buffer) {
+  let offset = 3;
+  let dataOffset;
+
+  if (der[offset] == 0x21) {
+      dataOffset = offset + 2;
+  }
+  else {
+      dataOffset = offset + 1;
+  }
+  const r = der.slice(dataOffset, dataOffset + 32);
+  offset = offset + der[offset] + 1 + 1
+  if (der[offset] == 0x21) {
+      dataOffset = offset + 2;
+  }
+  else {
+      dataOffset = offset + 1;
+  }
+  const s = der.slice(dataOffset, dataOffset + 32);
+  return [r, s]
+}
+
+// const proof = {
+//                 "message": "owner=0x485fa1883a8ad8b09d69522a7f79162fc3eb8e80&url=https://gateway.irys.xyz/CitxkaD3hLpTMaLkW1pagb1b6xPEtB6VvAjvfn6upyiZ",
+//                 "owner": "0x485fa1883a8ad8b09d69522a7f79162fc3eb8e80",
+//                 "secp256r1_pubkey": "BALNspjpyeYwX9vArRtgHOOltyOYYMDDOiw3G6xiyAzEBrGfr6RXQp32J18GmvA+FifRGLGohYVdlisxZrPHD74=",
+//                 "signature": "MEUCIQCMEZ7cBKAbBwqpNJ2w1uX8TESccJmL3Dn8BDFw1L3jWQIgE+OpfU3qsTlUP2JqMDVFKwojWJouQ6kUjtQqGrxb94w=",
+//                 "url": "https://gateway.irys.xyz/CitxkaD3hLpTMaLkW1pagb1b6xPEtB6VvAjvfn6upyiZ"
+//             }
+
+interface Proof {
+  message: string
+  owner: string
+  secp256r1_pubkey: string
+  signature: string
+  url: string
+}
+
+
+async function mintCamNFT(proof: Proof) {
+    
+    const { r, s } = await extract(proof.secp256r1_pubkey, proof.signature)
+
+    // const tx = await alpha.safeMint(toAddress, tokenURI);
+    // const receipt = await tx.wait();
+    
+    // console.log(`NFT minted successfully! Transaction hash: ${receipt.hash}`);
+    // // Parse the logs using abi to find the Transfer event and extract tokenId
+    // const parsedLog = alpha.interface.parseLog(receipt.logs[0])
+    // const tokenId = parsedLog?.args.tokenId;
+    // console.log(`Minted token ID: ${tokenId}`);
+    const extracted = {
+      r: "0x" + r.toString('hex'),
+      s: "0x" + s.toString('hex'),
+      pubkey: Buffer.from(proof.secp256r1_pubkey, 'base64')
+  }
+
+  const tx = await minter.mint(extracted.r, extracted.s, extracted.pubkey, proof.owner, proof.url);
+  await tx.wait(1)
+  console.log(tx)
+  return tx
+  // return { hash: receipt.hash, tokenId: Number(tokenId) };
+}
+
+
+export { mintCamNFT };
